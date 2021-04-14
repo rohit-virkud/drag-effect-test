e0e4ed310b0abe03cec9517b01041f8ae8c4f0d9
rohit-virkud <60460454+rohit-virkud@users.noreply.github.com>
2021-04-14 20:32:38 +0530

    Update elastiStack.js

--- elastiStack.js
+++ elastiStack.js
@@ -50,9 +50,9 @@
 
 	ElastiStack.prototype.options = {
 		// distDragBack: if the user stops dragging the image in a area that does not exceed [distDragBack]px for either x or y then the image goes back to the stack 
-		distDragBack : 100,
+		distDragBack : 200,
 		// distDragMax: if the user drags the image in a area that exceeds [distDragMax]px for either x or y then the image moves away from the stack 
-		distDragMax : 250,
+		distDragMax : 450,
 		// callback
 		isRandom : false , 
 		onUpdateStack : function( current ) { return false; }
